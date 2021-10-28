6fe5b8cdfd97698859699be29a4be8fad156d900
Hashim Qureshi <koukococoa@gmail.com>
2021-10-28 09:57:35 -0400

    Fix duplicate app.listen(), no more hard-coding DB information
    The username and password have since been changed as well as the database connection string so it cannot be accessed c:

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const appRoutes = require('./routes/appRoute');
+const dotenv = require("dotenv");
 
 const app = express();
 app.set('view engine', 'ejs');
+dotenv.config();
 
-mongoose.connect('mongodb+srv://ultimateUser:WScyh50KwhjyvRRX@fairychess.qcf83.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_POINTER}`, 
                 {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
-    app.listen(process.env.PORT || 3001, ()=>{ 
-        console.log('Server is running on port', 3001);
+    app.listen(process.env.PORT || 3000, () => { 
+        console.log('Server is running on port', 3000);
     });
 })
 .catch(err=>console.log(err.message));
 
 app.use(express.static('public'));
 app.use(morgan('tiny'));
-app.listen(process.env.PORT || 3000);
 
 app.use('/', appRoutes);
