ee6b71896b2814b80be272fe688016e6a2b58c67
Hashim Qureshi <koukococoa@gmail.com>
2021-10-27 21:08:59 -0400

    Add nodemon, remove app.listen() code

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 const app = express();
-
-let port = 3000;
-let host = 'localhost';
 app.set('view engine', 'ejs');
-
-app.listen(port, host, () =>
-{
-    console.log("FairyChess V1.0 is running on port ", port);
-});
 
 app.use(express.static('public'));
 app.use(morgan('tiny'));
